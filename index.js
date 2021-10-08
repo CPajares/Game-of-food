fec3691dd4bc28cda8b9f06eb633a3ffd8ef2dda
Carlos Pajares <c.pajares@hotmail.com>
2021-10-08 18:44:20 +0200

    created checkcell function first version

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
-const tableroInicial = [
-  [0, 0, 0, 0, 0],
-  [0, 0, 1, 0, 0],
+const originGame = [
+  [1, 1, 0, 0, 0],
+  [1, 0, 1, 0, 0],
   [0, 0, 1, 0, 0],
   [0, 0, 1, 0, 0],
   [0, 0, 0, 0, 0],
 ];
 
-console.log();
+function transformGame(game) {}
+
+function checkCell(game, x, y) {
+  let contadorVecinos = null;
+  if (x - 1 >= 0 && y - 1 >= 0 && game[x - 1][y - 1] === 1) {
+    contadorVecinos++;
+  }
+  if (x - 1 >= 0 && game[x - 1][y] === 1) {
+    contadorVecinos++;
+  }
+  if (x - 1 >= 0 && y + 1 <= 4 && game[x - 1][y + 1] === 1) {
+    contadorVecinos++;
+  }
+  if (y - 1 >= 0 && game[x][y - 1] === 1) {
+    contadorVecinos++;
+  }
+  if (y + 1 <= 4 && game[x][y + 1] === 1) {
+    contadorVecinos++;
+  }
+  if (x + 1 <= 4 && y - 1 >= 0 && game[x + 1][y - 1] === 1) {
+    contadorVecinos++;
+  }
+  if (x + 1 <= 4 && game[x + 1][y] === 1) {
+    contadorVecinos++;
+  }
+  if (x + 1 <= 4 && y + 1 <= 4 && game[x + 1][y + 1] === 1) {
+    contadorVecinos++;
+  }
+  return contadorVecinos;
+}
+
+checkCell(originGame, 0, 0);
+console.log(contadorVecinos);
